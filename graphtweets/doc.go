c349e2b5ba7c1eac8a54e039c3187f0dacc17fe1
JohnCoene <jcoenep@gmail.com>
2019-04-02 23:23:30 +0200

    restructure + package doc

---
+++ graphtweets/doc.go
@@ -0,0 +1,24 @@
+/*
+Package graphtweets provides an easy API to construct Twitter graphs.
+First set yourself up with a go-twitter client
+	// get tweets with go-twitter
+	config := oauth1.NewConfig("consumerKey", "consumerSecret")
+	token := oauth1.NewToken("accessToken", "accessSecret")
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	// Search Tweets
+	search, _, _ := client.Search.Tweets(&twitter.SearchTweetParams{
+		Query: "#golang",
+	})
+Import the package with
+	"go-graphtweets/graphtweets"
+Then simply build retweets and mentions graphs with:
+	mentionsGraph := graphtweets.GetMentionsGraph(*search) // build mentions graph
+	retweetGraph := graphtweets.GetRetweetGraph(*search) // build retweet graph
+You can also convert the resulting graphs to a wider, more JSON-friendly format
+	wide := graphtweets.ToWide(&mentionsGraph)
+*/
+package graphtweets
